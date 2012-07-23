d816e2d542d1c913d2778d37d105c87cc2a2e31c
Tangerine Cat <kaineer@gmail.com>
2012-07-23 17:30:17 +0600

    Debugging

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -3,13 +3,18 @@ module Admins
   class RegistrationsController < Devise::RegistrationsController
     respond_to :html, :json
 
+    # TODO: y u do not run this action?
+
     # GET /users/sign_up
     def sign_up
+      binding.pry
+
       @user ||= User.new
       @user.email = "foo@bar.baz"
 
       respond_with do |format|
         format.json { render :json => @user.sign_up_hash }
+        format.html
       end
     end
   end
